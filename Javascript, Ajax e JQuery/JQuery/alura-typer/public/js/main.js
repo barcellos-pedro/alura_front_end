a1fef983679902c1c3698cd0c90541d8d399a9f6
Pedro Barcellos <pedrobarcellosdosreis@gmail.com>
2020-03-17 16:43:16 -0300

    Fim da Aula03 - Escutando eventos com JQuery.

--- Javascript, Ajax e JQuery/JQuery/alura-typer/public/js/main.js
+++ Javascript, Ajax e JQuery/JQuery/alura-typer/public/js/main.js
@@ -1,12 +1,15 @@
 var frase = $(".frase").text(); // $ é igual jQuery, a variável irá retornar um objeto jQuery que contém nosso elemento
 var numPalavras = frase.split(" ").length; // quebra a frase sempre que houve espaços e guarda em um Array '[]' a quantidade de palavras a cada 'espaço'
-var tamanhoFrase = $("#tamanho-frase").text(numPalavras);
+var tamanhoFrase = $("#tamanho-frase");
+
+tamanhoFrase.text(numPalavras);
 
 var campo = $(".campo-digitacao");
-campo.on("input", function(){
+campo.on("input", ()=>{
     var conteudo = campo.val(); // val é abreviação de 'value', val pega conteudo das tags de 'Input'
     var qtdPalavras = conteudo.split(/\S+/).length-1; // Expressão regular que busca qualquer tipo de espaço vazio
-    var qtdCaracteres = conteudo.length; // quantidade de caracteres do textearea
+    var conteudoSemEspaco = conteudo.replace(/\s+/g,''); // Retira os espaço da String 
+    var qtdCaracteres = conteudoSemEspaco.length; // Quantidade de caracteres do textearea
     
     $("#contador-palavras").text(qtdPalavras);
     $("#contador-caracteres").text(qtdCaracteres);
