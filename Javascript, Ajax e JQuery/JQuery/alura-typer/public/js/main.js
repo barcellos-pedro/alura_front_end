206054ecbfda96629843e3f4764251573b5130dc
Pedro Barcellos <pedrobarcellosdosreis@gmail.com>
2020-03-24 18:12:12 -0300

    Adcionar função que reinicia o jogo e organizar o restante do código em funções.

--- Javascript, Ajax e JQuery/JQuery/alura-typer/public/js/main.js
+++ Javascript, Ajax e JQuery/JQuery/alura-typer/public/js/main.js
@@ -1,30 +1,53 @@
-var frase = $(".frase").text(); // $ é igual jQuery, a variável irá retornar um objeto jQuery que contém nosso elemento
-var numPalavras = frase.split(" ").length; // quebra a frase sempre que houve espaços e guarda em um Array '[]' a quantidade de palavras a cada 'espaço'
-var tamanhoFrase = $("#tamanho-frase");
-
-tamanhoFrase.text(numPalavras);
-
+var tempoInicial = $("#tempo-digitacao").text() // essa variavel não será alterada no cronometro
 var campo = $(".campo-digitacao");
-campo.on("input", ()=>{
-    var conteudo = campo.val(); // val é abreviação de 'value', val pega conteudo das tags de 'Input'
-    var qtdPalavras = conteudo.split(/\S+/).length-1; // Expressão regular que busca qualquer tipo de espaço vazio
-    var conteudoSemEspaco = conteudo.replace(/\s+/g,''); // Retira os espaço da String 
-    var qtdCaracteres = conteudoSemEspaco.length; // Quantidade de caracteres do textearea
-    
-    $("#contador-palavras").text(qtdPalavras);
-    $("#contador-caracteres").text(qtdCaracteres);
-    
+
+$(function(){ // Igual a $(document).ready(), roda quando a página for carregada completamente
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    $("#botao-reiniciar").click(reiniciaJogo);
 });
 
-var tempoRestante = $("#tempo-digitacao").text()
-campo.one("focus",()=>{ // focus considera o foco mesmo com dando tab no teclado // .one funciona somente uma vez
-    var cronometroId = setInterval(()=>{
-        tempoRestante--;
-        $("#tempo-digitacao").text(tempoRestante);
+function atualizaTamanhoFrase() {
+    var frase = $(".frase").text(); // $ é igual jQuery, a variável irá retornar um objeto jQuery que contém nosso elemento
+    var numPalavras = frase.split(" ").length; // quebra a frase sempre que houve espaços e guarda em uma variavel a quantidade de palavras a cada 'espaço'
+    var tamanhoFrase = $("#tamanho-frase");
+    tamanhoFrase.text(numPalavras);
+}
 
-        if(tempoRestante<1){
-            campo.attr("disabled", true);
-            clearInterval(cronometroId);
-        }
-    },1000);
-});
+function atualizaContadores() {
+    campo.on("input", () => {
+        var conteudo = campo.val(); // val é abreviação de 'value', val pega conteudo das tags de 'Input'
+        var qtdPalavras = conteudo.split(/\S+/).length - 1; // Expressão regular que busca qualquer tipo de espaço vazio
+
+        var conteudoSemEspaco = conteudo.replace(/\s+/g, ''); // Expressão regular que retira os espaço da String
+        var qtdCaracteres = conteudoSemEspaco.length; // Quantidade de caracteres do textearea
+
+        $("#contador-palavras").text(qtdPalavras);
+        $("#contador-caracteres").text(qtdCaracteres);
+    });
+};
+
+function inicializaCronometro() {
+    var tempoRestante = $("#tempo-digitacao").text()
+    campo.one("focus", () => { // focus considera o foco mesmo com dando tab no teclado // .one funciona somente uma vez
+        var cronometroId = setInterval(() => {
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+
+            if (tempoRestante < 1) {
+                campo.attr("disabled", true); // adiciona um novo atributo no textarea, e como o 'disabled' não possui valor precisamos setar se é true ou false
+                clearInterval(cronometroId);
+            }
+        }, 1000);
+    });
+};
+
+function reiniciaJogo() {
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro();
+};
