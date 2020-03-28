11baef7d04298f82c284ca515a63282e32529db3
Pedro Barcellos <pedrobarcellosdosreis@gmail.com>
2020-03-27 21:58:51 -0300

    Criar função novaLinha.

--- Javascript, Ajax e JQuery/JQuery/alura-typer/public/js/main.js
+++ Javascript, Ajax e JQuery/JQuery/alura-typer/public/js/main.js
@@ -30,7 +30,7 @@ function inicializaContadores() {
 };
 
 function inicializaCronometro() {
-    var tempoRestante = $("#tempo-digitacao").text()
+    var tempoRestante = $("#tempo-digitacao").text();
     campo.one("focus", () => { // focus considera o foco mesmo com dando tab no teclado // .one funciona somente uma vez
         var cronometroId = setInterval(() => {
             tempoRestante--;
@@ -77,19 +77,37 @@ function inserePlacar() {
     var corpoTabela = $(".placar").find("tbody"); // vai na section e depois procura a tbody dentro dela
     var usuario = "Pedro";
     var numPalavras = $("#contador-palavras").text();
-    var botaoRemover = "<a href='#'><i class='small material-icons'>delete</i></a>";
-    var linha = "<tr>" +
-                    "<td>" + usuario + "</td>" +
-                    "<td>" + numPalavras + "</td>" +
-                    "<td>" + botaoRemover + "</td>"+
-                "</tr>";
+    // Não é possível atrelar evento de remover para string com elementos HTML, por isso é necessário CRIAR UM ELEMENTO HTML com o jQuery (função novaLinha())
+    var linha = novaLinha(usuario,numPalavras);
+    linha.find(".botao-remover").click(removeLinha);
     corpoTabela.prepend(linha); // prepend adiciona como elemento filho no início da tabela(inverso do append)
 }
 
-$(".botao-remover").click(function(event){ // passa o event para em seguida previnir a ação padrão (linha seguinte) do href de subir para o topo da página
+function novaLinha(usuario, palavras){
+    var linha = $("<tr>"); // Criando elemento HTML de fato com jQuery, ao invés de criar em uma STRING ao estilo innerHTML
+    var colunaUsuario = $("<td>").text(usuario);
+    var colunaPalavras = $("<td>").text(palavras);
+    var colunaRemover = $("<td>");
+
+    var link = $("<a>").attr("href","#").addClass("botao-remover");
+    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+    
+    link.append(icone);
+
+    colunaRemover.append(link);
+
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;
+}
+
+function removeLinha(event){ // passa o event para em seguida previnir a ação padrão (linha seguinte) do href de subir para o topo da página
     event.preventDefault();
-    $(this).parent().parent().remove(); // this não funcionou com arrow function
-});
+    // Envolvemos o this no seletor $() para ele poder ter os poderes do jQuery
+    $(this).parent().parent().remove(); // this não funcionou com arrow function // parent 2x para poder subir para a TR, a<td<tr
+};
 
 function reiniciaJogo() {
     campo.attr("disabled", false);
