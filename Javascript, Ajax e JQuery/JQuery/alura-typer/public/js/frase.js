4051ce46d4fcd56aa65da7986081c87627f97fd1
Pedro Barcellos <pedrobarcellosdosreis@gmail.com>
2020-03-30 22:11:38 -0300

    Adicionar função GET com AJAX para trocar frase.

--- Javascript, Ajax e JQuery/JQuery/alura-typer/public/js/frase.js
+++ Javascript, Ajax e JQuery/JQuery/alura-typer/public/js/frase.js
@@ -1,5 +1,12 @@
 $("#botao-frase").click(fraseAleatoria);
 
-function fraseAleatoria(){
-    
+function fraseAleatoria() {
+    $.get("http://localhost:3000/frases", trocaFraseAleatoria)
+
+}
+
+function trocaFraseAleatoria(data) {    // Passa o argumento 'data' que guarda/retorna o conteúdo da requisição com vários objetos
+    var frase = $(".frase");
+    var numeroAleatorio = Math.floor(Math.random() * data.length);
+    frase.text(data[numeroAleatorio].texto);
 }
