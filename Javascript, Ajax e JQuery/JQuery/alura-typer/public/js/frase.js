7069a37b7b8970de77f47eb3df6c672c06fed1d1
Pedro Barcellos <pedrobarcellosdosreis@gmail.com>
2020-04-03 19:47:13 -0300

    Adicionar Spinner de carregamento da frase aleatória.

--- Javascript, Ajax e JQuery/JQuery/alura-typer/public/js/frase.js
+++ Javascript, Ajax e JQuery/JQuery/alura-typer/public/js/frase.js
@@ -1,12 +1,15 @@
 $("#botao-frase").click(fraseAleatoria);
 
 function fraseAleatoria() {
+    $("#spinner").toggle();
     $.get("http://localhost:3000/frases", trocaFraseAleatoria)
     .fail(()=>{
       $('#erro').show();
       setTimeout(()=>{$('#erro').toggle()},2000)
     })
-
+    .always(()=>{
+      $("#spinner").toggle();  // Independente da requisição ser feita com sucesso (.get) ou falhar (.fail) o .always sempre será executado
+    })
 }
 
 function trocaFraseAleatoria(data) {    // Passa o argumento 'data' que guarda/retorna o conteúdo da requisição com vários objetos
